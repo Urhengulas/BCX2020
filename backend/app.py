bca0086ba2f2a4910051ea12bcbe0563e22ba60f
Emil9999 <emil.sallem@code.berlin>
2020-02-18 17:56:06 +0100

    Added light controls

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect
+import requests
 from authlib.integrations.flask_client import OAuth
+import json
 # use loginpass to make OAuth connection simpler
 
 app = Flask(__name__)
@@ -15,7 +17,7 @@ oauth.register(
     authorize_url='https://api.home-connect.com/security/oauth/authorize',
     authorize_params=None,
     api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'CoffeeMaker-Control'},
+    client_kwargs={'scope': 'CoffeeMaker-Control IdentifyAppliance CoffeeMaker-Monitor CoffeeMaker-Settings'},
     
 )
 
@@ -23,18 +25,60 @@ homeConnect = oauth.create_client('minion-production')
 
 @app.route('/')
 def hello_world():
-
+    for i in range(13, -13, -1):
+        i = abs(i)
+        changeLight(hex(i*21).lstrip("0x"))
     return 'Hello, World!'
 
 @app.route('/login')
 def login():
-    homeConnect = oauth.create_client('minion-production')
-    redirect_uri = 'https://example.com'
+    print('Hello, World!')
+    redirect_uri ="http://localhost:5000/authorize"
     return homeConnect.authorize_redirect(redirect_uri)
+    
+
+@app.route('/authorize')
 def authorize():
-    github = oauth.create_client('github')
-    token = oauth.github.authorize_access_token(request)
-    resp = oauth.github.get('user')
-    profile = resp.json()
+    token = homeConnect.authorize_access_token()
+    
+    #r = homeConnect.get('https://simulator.home-connect.com/api/homeappliances')
+    status = homeConnect.get("https://api.home-connect.com/api/homeappliances/SIEMENS-TI9575X1DE-68A40E357F21/status")
+    availablePrograms = homeConnect.get('https://api.home-connect.com/api/homeappliances/SIEMENS-TI9575X1DE-68A40E357F21/programs/available')
+    print(status.content)
+    selectedProgram()
+    
+
+
     # do something with the token and profile
-    return '...'
+    return redirect('/')
+
+def makeCoffee():
+    makeCoffee = homeConnect.put('https://api.home-connect.com/api/homeappliances/SIEMENS-TI9575X1DE-68A40E357F21/programs/active', json={"data":{
+        "key":"ConsumerProducts.CoffeeMaker.Program.Beverage.Espresso",
+        "options":[
+            {
+                "key":"ConsumerProducts.CoffeeMaker.Option.BeanAmount",
+                "value":"ConsumerProducts.CoffeeMaker.EnumType.BeanAmount.Mild"
+            },
+            {
+                "key":"ConsumerProducts.CoffeeMaker.Option.FillQuantity",
+                "value":35,
+                "unit":"ml"
+            }
+        ]
+    }})
+    print(makeCoffee.content)
+    return
+
+def selectedProgram():
+    selectedProgram = homeConnect.get("https://api.home-connect.com/api/homeappliances/SIEMENS-TI9575X1DE-68A40E357F21/programs/selected")
+    print(selectedProgram.content)
+    return
+
+def initIOPort():
+    r = requests.post('http://192.168.1.1/TMG.htm', data ="UDP_Packet=24.00.02.0F.0.00.0C.11.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.20.20")
+
+
+def changeLight(hexvalue):
+    r = requests.post('http://192.168.1.1/TMG.htm', data ="UDP_Packet=24.00.02.0B.0."+hexvalue+".00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.20.20")
+
